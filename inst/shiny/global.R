3a523d6cd7e1ed59fd236eaad9b1c522f3b6b12e
Jamie Gilbert <JGilber2@its.jnj.com>
2023-02-08 11:40:47 -0800

    Global.R for shiny changed

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -1,18 +1,12 @@
 # details of results database
 # Created for data.ohdsi.org
-# connectionDetails <- DatabaseConnector::createConnectionDetails(
-#   dbms = "postgresql",
-#   user = Sys.getenv("phenotypeLibrarydbUser"),
-#   password = Sys.getenv("phenotypeLibrarydbPw"),
-#   server = paste0(Sys.getenv("phenotypeLibraryServer"), "/", Sys.getenv("phenotypeLibrarydb")),
-#   port = 5432
-# )
-#
-# # params
-# # resultsSchema <- "reward_truven_ccae_v1676"
-# resultsSchema <- "comparator_selector"
-similarityTable <- "cosine_similarity"
-covDataTable <- "covariate_mean"
-databaseTable <- "cdm_source_info"
+# details of results database
+connectionDetails <- DatabaseConnector::createConnectionDetails(
+  dbms = "postgresql",
+  user = Sys.getenv("phenotypeLibrarydbUser"),
+  password = Sys.getenv("phenotypeLibrarydbPw"),
+  server = paste0(Sys.getenv("phenotypeLibraryServer"), "/", Sys.getenv("phenotypeLibrarydb")),
+  port = 5432
+)
 
 connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "sqlite", server = "test.sqlite")
