a66a1ba7b9e767b1ce9b7a8e033d33637c4cd5d3
Jamie Gilbert <JGilber2@its.jnj.com>
2023-02-08 17:00:39 -0800

    fixes for atc level sql

--- inst/shiny/global.R
+++ inst/shiny/global.R
@@ -8,12 +8,3 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(
   server = paste0(Sys.getenv("phenotypeLibraryServer"), "/", Sys.getenv("phenotypeLibrarydb")),
   port = 5432
 )
-
-
-connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = "postgresql",
-                                                                server = "reward.cterqq54xyuu.us-east-1.rds.amazonaws.com/rewardb_dev",
-                                                                user = "reward_admin",
-                                                                password = keyring::key_get("reward-db", user = "reward_admin"),
-                                                                port = 5432)
-resultsSchema <- "cse_022023"
-tablePrefix <- "cse_"
